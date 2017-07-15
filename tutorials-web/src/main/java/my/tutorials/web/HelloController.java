b139042c7ee49124d89031faff1d80c469df395d
zsp <zsp@zsp-1>
2017-07-15 23:03:35 +0800

    web tutorials

---
+++ tutorials-web/src/main/java/my/tutorials/web/HelloController.java
@@ -0,0 +1,16 @@
+package my.tutorials.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+	
+	@RequestMapping(value = "hello", method = RequestMethod.GET)
+	public String say() {
+		return "Hi";
+	}
+
+}
